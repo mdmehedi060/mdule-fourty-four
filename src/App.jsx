72a3c2b4f0e00f8d6ed10354d6b747fec5e58b65
mamehedi060 <mamehedi060@gmail.com>
2023-09-19 21:03:11 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+// import Daysinav from './Components/Daysinav/Daysinav'
+import Navbar from './Components/Navbar/Navbar'
+
+function App() {
+  
+
+  return (
+    <>
+      {/* <Daysinav></Daysinav> */}
+      <Navbar></Navbar>
+      <h1 className='text-7xl bg-orange-600 text-center'>Vite + React</h1>
+    
+
+
+    </>
+  )
+}
+
+export default App
